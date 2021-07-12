4cad11aecd5b0bac6af509b30ff4d8d1c9149865
twong4 <62853188+twong4@users.noreply.github.com>
2021-07-12 17:46:49 -0400

    custom validation regex matches upper and lower case

--- src/app/custom-validators/avoidWord.ts
+++ src/app/custom-validators/avoidWord.ts
@@ -2,7 +2,7 @@ import { AbstractControl, ValidatorFn } from "@angular/forms";
 
 export function avoidWord(regex: RegExp): ValidatorFn {
     return (formControl: AbstractControl): {[errorName: string]: any}|null => {
-        const notAllowed = regex.test(formControl.value);
+        const notAllowed = regex.test(formControl.value.toLowerCase());
         if (notAllowed) {
             return {
                 prohibited: {value: formControl.value}
